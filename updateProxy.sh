bcd7761ed9fc94136434eeefa27d8485387670a9
tom <tom.chalker@canada.ca>
2020-05-07 10:45:24 -0400

    Set static root with env var

--- updateProxy.sh
+++ updateProxy.sh
@@ -3,6 +3,6 @@ set -eu
 
 sed "s/{\$EXTERNAL_API_SVC}/$EXTERNAL_API_SVC/g" /app/Caddyfile.template > /app/Caddyfile
 
-ln -s /app/packagehtml/$HTML_ROOT /app/html 
+#ln -s /app/packagehtml/$HTML_ROOT /www/html
 
 exec caddy run
